158bbb76c36f97f4f60d75441f46727fa9aea57c
Andrey Grytsenko <andreyapg@gmail.com>
2022-05-22 10:35:35 +0300

    Reflection: refactoring 2022-05-22

---
+++ pr_v1_1.py
@@ -0,0 +1,25 @@
+# Hacking Version 1
+# This is a text-based password guessing game that displays a
+# list of potential computer passwords. The player is allowed
+# 1 attempt to guess the password. The game indicates that the
+# player failed to guess the password correctly.
+
+# display header
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+# display password list
+print('PROVIDE\nSETTING\nCANTINA\nCUTTING\nHUNTERS\nSURVIVE\nHEARING\nHUNTING\nREALIZE\nNOTHING\nOVERLAP\nFINDING'
+      '\nPUTTING\n')
+
+# prompt for guess
+input('Enter password >')
+
+# end game
+#   display failure outcome
+print('LOGIN FAILURE - TERMINAL LOCKED\n\nPLEASE CONTACT AN ADMINISTRATOR\n')
+
+#   prompt for end
+input('PRESS ENTER TO EXIT')
+
