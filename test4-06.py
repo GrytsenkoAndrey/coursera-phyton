db901895ae1f6ee20bf884693a449bdc9f27d8fc
Andrey Grytsenko <andreyapg@gmail.com>
2022-05-28 12:09:39 +0300

    Python Assignment Statement #1 2022-05-28

---
+++ test4-06.py
@@ -0,0 +1,4 @@
+favorite = input('What is your favorite colour?\n')
+second_favorite = input('What is your second favorite color?\n')
+print('You picked')
+print(favorite + '\n and \n' + second_favorite)
