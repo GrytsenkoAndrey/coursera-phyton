9180ac7121ef42adb750949c5c022c1c1c69b917
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-06 11:01:56 +0300

    Update variant of questions (tasks) 2022-06-06

--- test4-06.py
+++ test4-06.py
@@ -1,4 +1,16 @@
-favorite = input('What is your favorite colour?\n')
-second_favorite = input('What is your second favorite color?\n')
-print('You picked')
-print(favorite + '\n and \n' + second_favorite)
+x = 'a'
+y = ord(x)
+z = print(y)
+test = 13
+
+#your_string = input('Enter a number >')
+#print(your_string)
+#your_int = int(your_string)
+#print(your_int)
+
+# favorite = input('What is your favorite colour?\n')
+# second_favorite = input('What is your second favorite color?\n')
+# print('You picked')
+# print(favorite + '\n and \n' + second_favorite)
+
+
