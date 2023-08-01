a2b466519cc8292b182600b7b9f4aaec591dc9ee
Andrey Grytsenko <andreyapg@gmail.com>
2023-08-01 12:05:08 +0300

    Add percintile 2023-08-01

---
+++ percentile.py
@@ -0,0 +1,25 @@
+a = [1, 2, 0, 10, 50, 42, 38]
+b = [1, 2, 0, 10, 50, 42, 38, 5]
+
+# avg a
+print(sum(a) / len(a))
+print('\n')
+# avg b
+print(sum(b) / len(b))
+print('\n')
+
+# mediana a
+sort_a = sorted(a)
+len(sort_a)
+sort_a[3]
+
+#mediana b
+sort_b = sorted(b)
+len(sort_b)
+
+sort_b[3]
+sort_b[4]
+
+(sort_b[3] + sort_b[4])/2
+
+
